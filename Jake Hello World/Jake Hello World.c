7c5859f96f5caa0196bafde4cb51e2058309384b
Jake Spencer <jake@uncarbonatedsoftware.com>
2015-04-16 22:59:21 -0500

    Initial Commit

---
+++ Jake Hello World/Jake Hello World.c
@@ -0,0 +1,16 @@
+// Created on Thu April 16 2015
+
+int main()
+{
+	printf("Hello, World");
+	
+	motor(0, 100);
+	motor(2, 100);
+	msleep(5000);
+	ao();
+	
+	printf("Hello, kyle");
+
+
+	return 0;
+}
